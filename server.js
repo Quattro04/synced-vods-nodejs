cc92869e3bb7a191f9bda6b9af9f6f328eca7c22
Matija Jeras <matija.jeras64@gmail.com>
2021-07-07 01:24:33 +0200

    timestamp receive/send

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ const wss = new Server({ server });
 wss.on('connection', (ws) => {
     console.log('Client connected');
     ws.on('close', () => console.log('Client disconnected'));
+    ws.on('message', timestamp => {
+        wss.clients.forEach(client => {
+            client.send(timestamp);
+        });
+    });
 });
 
-setInterval(() => {
-    wss.clients.forEach((client) => {
-        client.send(new Date().toTimeString());
-    });
-}, 1000);
+// setInterval(() => {
+//     wss.clients.forEach((client) => {
+//         client.send(new Date().toTimeString());
+//     });
+// }, 1000);
