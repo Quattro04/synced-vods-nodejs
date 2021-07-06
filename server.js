153b1ecf164c98dc666cab7f85d8299bf4de667e
Matija Jeras <matija.jeras64@gmail.com>
2021-07-06 21:13:52 +0200

    deploy test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = express()
 const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
-    console.log('Client has connected');
+    console.log('Client connected');
     ws.on('close', () => console.log('Client disconnected'));
 });
 
